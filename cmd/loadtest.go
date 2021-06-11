3c7a0096482a8c5096eb4dbf6297e5bbb89105e6
Vinod Tahelyani <tahelyanivinod@gmail.com>
2021-06-11 20:22:52 +0530

    Add main file

---
+++ cmd/loadtest.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"flag"
+	"fmt"
+
+	"loadtest.github.com/lib"
+)
+
+var cmdLineOptions lib.Options = lib.NewOptions()
+
+func init()  {
+
+	flag.StringVar(&cmdLineOptions.Method, "method", "GET", "method to url")
+	flag.StringVar(&cmdLineOptions.Method, "m", "GET", "method to url")
+
+	flag.StringVar(&cmdLineOptions.Body, "body", "", "Send string as Request body")
+	flag.StringVar(&cmdLineOptions.Body, "b", "", "Send string as Request body")
+ 
+	flag.StringVar(&cmdLineOptions.File, "file", "", "Send the contents of the file as Request body")
+	flag.StringVar(&cmdLineOptions.File, "f", "", "Send the contents of the file as Request body")
+
+	flag.IntVar(&cmdLineOptions.MaxRequests, "maxRequests", -1, "Number of requests to perform")
+	flag.IntVar(&cmdLineOptions.MaxRequests, "n", -1, "Number of requests to perform")
+
+	flag.IntVar(&cmdLineOptions.Concurrency, "concurrency", 1, "Number of requests to make")
+	flag.IntVar(&cmdLineOptions.Concurrency, "c", 1, "Number of requests to make")
+
+	flag.IntVar(&cmdLineOptions.RequestPerSecond, "rps", 1, "Specify the requests per second for each client")
+
+	flag.Var(cmdLineOptions.RequestHeaders, "headers", "Send a header as header:value (multiple)")
+	flag.Var(cmdLineOptions.RequestHeaders, "h", "Send a header as header:value (multiple)")
+
+	flag.Var(cmdLineOptions.Cookies, "cookie", "Send a cookie as name=value (multiple)")
+	flag.Var(cmdLineOptions.Cookies, "c", "Send a cookie as name=value (multiple)")
+
+}
+
+func Parse()  {
+	flag.Parse()
+	extraArgs := flag.Args()
+	if len(extraArgs) == 0 {
+		lib.HelpAndExit("Missing URL to load-test")
+	} else {
+		cmdLineOptions.URL = extraArgs[0]
+		flag.CommandLine.Parse(extraArgs[1:])
+		extraArgs = append([]string{}, cmdLineOptions.URL)
+		extraArgs = append(extraArgs, flag.CommandLine.Args()...)
+		if len(extraArgs) > 1 {
+			lib.HelpAndExit("Too many arguments: ", fmt.Sprint(extraArgs))
+		}
+	}
+}
+
+func Loadtest()  {
+	Parse()
+	cmdLineOptions.ExecuteTest()
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"loadtest.github.com/cmd"
+)
+
+func main()  {
+	cmd.Loadtest()
+}
